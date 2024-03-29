42046726f6d46af8c4117d9a3a4bca7f705ce375
ctpanh <ctpanh03@gmail.com>
2024-03-29 20:57:20 +0700

    feat: library/playlist, library/upload

---
+++ client/src/app/library/playlist/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 p-4 text-neutral-400 text-xl">
+      <Image
+        src={
+          "https://zmp3-static.zmdcdn.me/skins/zmp3-v6.1/images/icons/empty-fav-song-dark.png"
+        }
+        alt="empty-fav-song"
+        width={200}
+        height={200}
+      />
+      Tính năng tạo danh sách đang phát triển!
+    </div>
+  );
+}
