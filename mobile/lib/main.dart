41f4ae3fdea4b62b0682b27e3cea4203788f3fcf
Pham Minh Tam <100601043+21020391@users.noreply.github.com>
2024-04-09 05:21:07 +0700

    create signin, signup screens

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/signin.dart';
+import 'package:mobile/signup.dart';
 
 void main() {
-  runApp(
-   MaterialApp(
-      title: 'SoundShelter',
-      home: SoundShelterHome(),
-      theme: ThemeData(
-        primaryColor: Color(0xFFB2572B),
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Color(0xFFB2572B),
-        ),
+  runApp(MaterialApp(
+    title: 'SoundShelter',
+    home: const SoundShelterHome(),
+    theme: ThemeData(
+      primaryColor: const Color(0xFFB2572B),
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        primary: const Color(0xFFB2572B),
+        background: const Color(0xFFD6ACAE),
       ),
     ),
-  );
+  ));
 }
 
 class SoundShelterHome extends StatelessWidget {
@@ -21,29 +22,100 @@ class SoundShelterHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: const Text('SoundShelter'),
-        actions: const [
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFD6ACAE),
+          leading: const IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
             onPressed: null,
           ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Hello, world!'),
-      ),
-      floatingActionButton: const FloatingActionButton(
-        tooltip: 'Add',
-        onPressed: null,
-        child: Icon(Icons.add),
-      ),
-    );
+          title: Text(
+            'SoundShelter',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+          actions: const [
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null,
+            ),
+          ],
+        ),
+        body: SafeArea(
+            child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.all(30.0),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/guitar.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Column(children: <Widget>[
+                  Text(
+                    "Chào mừng",
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SigninPage()));
+                    },
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                            color: Theme.of(context).colorScheme.primary),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Text(
+                      "Đăng nhập",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignupPage()));
+                    },
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                            color: Theme.of(context).colorScheme.primary),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Text(
+                      "Đăng ký",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18),
+                    ),
+                  ),
+                ]),
+              ]),
+        )));
   }
 }
