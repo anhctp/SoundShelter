a4d692377dfdd4954aa45a0a8f96c2041e232288
Pham Minh Tam <100601043+21020391@users.noreply.github.com>
2024-05-26 17:39:17 +0700

    fix hub screen

--- mobile/lib/components/hub_cart/grid_cards.dart
+++ mobile/lib/components/hub_cart/grid_cards.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/module/hub-screen/detail_genre_screen.dart';
 import 'package:mobile/components/hub_cart/hub_card.dart';
-import 'package:mobile/provider/album_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:mobile/model/album_model.dart';
 
 class GridCards extends StatefulWidget {
-  final int genreId;
-  final String label;
-  const GridCards({super.key, required this.genreId, required this.label});
+  final Widget detailChild;
+  final List<Album> items;
+  const GridCards({super.key, required this.items, required this.detailChild});
 
   @override
   State<GridCards> createState() => _GridCardsState();
@@ -15,15 +13,7 @@ class GridCards extends StatefulWidget {
 
 class _GridCardsState extends State<GridCards> {
   @override
-  void initState() {
-    super.initState();
-    final albumProvider = Provider.of<AlbumProvider>(context, listen: false);
-    albumProvider.getAlbumsByGenre(widget.genreId);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Consumer<AlbumProvider>(builder: (context, albumProvider, child) {
       return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 5.0),
         child:
@@ -37,12 +27,13 @@ class _GridCardsState extends State<GridCards> {
                 );
               },
               scrollDirection: Axis.horizontal,
-              itemCount: albumProvider.albums.length < 5
-                  ? albumProvider.albums.length
+              itemCount: widget.items.length < 5
+                  ? widget.items.length
                   : 5, // 1 for the last index button
               shrinkWrap: true,
               physics: ClampingScrollPhysics(),
               itemBuilder: (BuildContext context, int index) {
+                final item = widget.items[index];
                 //last index button
                 return index == 4
                     ? TextButton(
@@ -51,35 +42,28 @@ class _GridCardsState extends State<GridCards> {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => DetailGenreScreen(
-                                      genreId: widget.genreId,
-                                      label: widget.label,
-                                    )),
+                                builder: (context) => widget.detailChild),
                           );
                         },
                         child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(
-                                Icons.arrow_forward,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              Icons.arrow_forward,
+                              color: Color(0xFFB2572B),
+                            ),
+                            Text(
+                              'Xem tất cả',
+                              style: TextStyle(
                                 color: Color(0xFFB2572B),
                               ),
-                              Text(
-                                'Xem tất cả',
-                                style: TextStyle(
-                                  color: Color(0xFFB2572B),
-                                ),
-                              ),
-                            ]),
+                            ),
+                          ],
+                        ),
                       )
-                    : HubCard(
-                        imageFilePath:
-                            albumProvider.albums[index].imageFilePath,
-                        albumTitle: albumProvider.albums[index].title,
-                      );
+                    : HubCard(imageFilePath: item.imageFilePath, albumTitle: item.title);
               }),
         ),
       );
-    });
-  }
+    }
 }
--- mobile/lib/provider/genre_provider.dart
+++ mobile/lib/provider/genre_provider.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/model/genre_model.dart';
+import 'package:mobile/service/album_service.dart';
 import 'package:mobile/service/genre_service.dart';
 
 class GenreProvider with ChangeNotifier {
   GenreService genreService = GenreService();
   List<Genre> _genres = [];
+  List<Map<String, dynamic>> _albums = [];
   //getter
   List<Genre> get genres => _genres;
+  List<Map<String, dynamic>> get albums => _albums;
+
   Future<void> getAllGenre() async {
     _genres = await genreService.getAllGenre();
     notifyListeners();
   }
 
-  Future<void> getGenreById(int id) async {
-    await genreService.getGenreById(id);
+  Future<void> getAlbums() async {
+    _genres = await genreService.getAllGenre();
+    _albums = [];
+    AlbumService albumService = AlbumService();
+    for (var genreItem in _genres) {
+      final album = await albumService.getAlbumsByGenre(genreItem.id);
+      if (album.isNotEmpty) {
+        _albums.add({
+          'genre': genreItem,
+          'album': album,
+        });
+      }
+    }
     notifyListeners();
   }
 }
