627228bcf7ce92368883b3bd84882b226537000e
Pham Minh Tam <100601043+21020391@users.noreply.github.com>
2024-05-27 19:28:23 +0700

    chore: divide file

--- mobile/lib/components/hub_cart/grid_cards.dart
+++ mobile/lib/components/hub_cart/grid_cards.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/components/hub_cart/hub_card.dart';
+import 'package:mobile/components/card/hub_card.dart';
 import 'package:mobile/model/album_model.dart';
 
 class GridCards extends StatefulWidget {
@@ -14,56 +14,58 @@ class GridCards extends StatefulWidget {
 class _GridCardsState extends State<GridCards> {
   @override
   Widget build(BuildContext context) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 5.0),
-        child:
-            //Horizontal Grid
-            Container(
-          height: 200,
-          child: ListView.separated(
-              separatorBuilder: (context, index) {
-                return SizedBox(
-                  width: 0,
-                );
-              },
-              scrollDirection: Axis.horizontal,
-              itemCount: widget.items.length < 5
-                  ? widget.items.length
-                  : 5, // 1 for the last index button
-              shrinkWrap: true,
-              physics: ClampingScrollPhysics(),
-              itemBuilder: (BuildContext context, int index) {
-                final item = widget.items[index];
-                //last index button
-                return index == 4
-                    ? TextButton(
-                        onPressed: () {
-                          print('Xem tat ca');
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => widget.detailChild),
-                          );
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(
-                              Icons.arrow_forward,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+      child:
+          //Horizontal Grid
+          Container(
+        height: 200,
+        child: ListView.separated(
+            separatorBuilder: (context, index) {
+              return SizedBox(
+                width: 0,
+              );
+            },
+            scrollDirection: Axis.horizontal,
+            itemCount: widget.items.length < 5
+                ? widget.items.length
+                : 5, // 1 for the last index button
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) {
+              final item = widget.items[index];
+              //last index button
+              return index == 4
+                  ? TextButton(
+                      onPressed: () {
+                        print('Xem tat ca');
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => widget.detailChild),
+                        );
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.arrow_forward,
+                            color: Color(0xFFB2572B),
+                          ),
+                          Text(
+                            'Xem tất cả',
+                            style: TextStyle(
                               color: Color(0xFFB2572B),
                             ),
-                            Text(
-                              'Xem tất cả',
-                              style: TextStyle(
-                                color: Color(0xFFB2572B),
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                    : HubCard(imageFilePath: item.imageFilePath, albumTitle: item.title);
-              }),
-        ),
-      );
-    }
+                          ),
+                        ],
+                      ),
+                    )
+                  : HubCard(
+                      imageFilePath: item.imageFilePath,
+                      albumTitle: item.title);
+            }),
+      ),
+    );
+  }
 }
