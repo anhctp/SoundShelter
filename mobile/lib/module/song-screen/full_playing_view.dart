aa3d69c0da8b1d271f952a14de66f36bde0e2912
Pham Minh Tam <100601043+21020391@users.noreply.github.com>
2024-06-06 18:00:20 +0700

    chore: divide file

--- mobile/lib/module/song-screen/full_playing_view.dart
+++ mobile/lib/module/song-screen/full_playing_view.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:mobile/components/box/neu_box.dart';
 import 'package:mobile/components/title/screen_header.dart';
 import 'package:mobile/module/comment/comment_modal.dart';
+import 'package:mobile/module/song-screen/duration_progress.dart';
+import 'package:mobile/module/song-screen/song_detail.dart';
 import 'package:mobile/module/timer-picker/timer_picker.dart';
 import 'package:mobile/provider/song_provider.dart';
 import 'package:provider/provider.dart';
@@ -16,245 +16,111 @@ class FullPlayingView extends StatefulWidget {
 }
 
 class _FullPlayingViewState extends State<FullPlayingView> {
-  //convert duratiob into min:sec
-  String formatTime(Duration duration) {
-    String twoDigiSeconds =
-        duration.inSeconds.remainder(60).toString().padLeft(2, '0');
-    String formattedTime = "${duration.inMinutes}:$twoDigiSeconds";
-
-    return formattedTime;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Consumer<SongProvider>(builder: (context, value, child) {
-      //get playlist
-      final songs = value.playingSongs;
+    return Consumer<SongProvider>(
+      builder: (context, value, child) {
+        //get playlist
+        final songs = value.playingSongs;
 
-      //get current song index
-      final currentSong = songs[value.currentSongIndex ?? 0];
+        //get current song index
+        final currentSong = songs[value.currentSongIndex ?? 0];
 
-      return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        appBar: ScreenHeader(title: "Bài hát đang phát"),
-        body: SafeArea(
-          child: SingleChildScrollView(
-            padding:
-                const EdgeInsets.only(left: 25, right: 25, bottom: 25, top: 25),
-            child: Column(
-              children: [
-                //song detail
-                Neubox(
-                  child: Column(
+        return Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          appBar: ScreenHeader(title: "Bài hát đang phát"),
+          body: SafeArea(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.only(
+                  left: 25, right: 25, bottom: 25, top: 25),
+              child: Column(
+                children: [
+                  //song detail
+                  SongDetail(currentSong: currentSong, songProvider: value),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  //song duration progress
+                  DurationProgress(songProvider: value),
+
+                  SizedBox(
+                    height: 25,
+                  ),
+
+                  //playback controls
+                  Row(
                     children: [
-                      //img
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.network(currentSong.imageFilePath),
+                      //skip prev
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: value.playPrevSong,
+                          child: Neubox(
+                            child: Icon(Icons.skip_previous),
+                          ),
+                        ),
                       ),
-
-                      //song and artist name and icon
-                      Padding(
-                        padding: EdgeInsets.all(15.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            //song and artist name
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  width: 240,
-                                  child: Text(
-                                    currentSong.title,
-                                    overflow: TextOverflow.clip,
-                                    maxLines: 2,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  width: 240,
-                                  child: Text(
-                                    currentSong.artist,
-                                    overflow: TextOverflow.clip,
-                                    maxLines: 2,
-                                  ),
-                                ),
-                              ],
-                            ),
-
-                            //more icon
-                            IconButton(
-                              onPressed: () {
-                                showModalBottomSheet(
-                                  context: context,
-                                  builder: (context) => Container(),
-                                );
-                              },
-                              icon: Icon(Icons.more_vert),
-                            ),
-                          ],
+                      const SizedBox(
+                        width: 20.0,
+                      ),
+                      //play pause
+                      Expanded(
+                        flex: 2,
+                        child: GestureDetector(
+                          onTap: value.pauseOrResume,
+                          child: Neubox(
+                            child: Icon(value.isPlaying
+                                ? Icons.pause
+                                : Icons.play_arrow),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20.0,
+                      ),
+                      //skip forward
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: value.playNextSong,
+                          child: Neubox(
+                            child: Icon(Icons.skip_next),
+                          ),
                         ),
                       ),
                     ],
                   ),
-                ),
-
-                const SizedBox(
-                  height: 25,
-                ),
-
-                //song duration progress
-                Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 25.0,
-                      ),
-                      child: Column(
+                  SizedBox(
+                    height: 25,
+                  ),
+                  //like,share,cmt
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              //start time
-                              Text(formatTime(value.currentDuration)),
-
-                              //shuffle
-                              IconButton(
-                                onPressed: value.shuffle,
-                                focusColor: Colors.blue,
-                                icon: Icon(
-                                  Icons.shuffle,
-                                ),
-                              ),
-
-                              //repeat icon
-                              IconButton(
-                                onPressed: value.repeat,
-                                icon: Icon(
-                                  Icons.repeat,
-                                  color: (value.isRepeat)
-                                      ? Colors.blue
-                                      : Colors.black,
-                                ),
-                              ),
-
-                              //end time
-                              Text(formatTime(value.totalDuration)),
-                            ],
+                          //like
+                          IconButton(
+                            onPressed: value.favorites,
+                            icon: (value.isFavorite)
+                                ? Icon(Icons.favorite, color: Colors.red)
+                                : Icon(Icons.favorite_border),
                           ),
-
-                          //song duration progress
-                          SliderTheme(
-                            data: SliderTheme.of(context).copyWith(
-                              thumbShape: const RoundSliderThumbShape(
-                                  enabledThumbRadius: 0),
-                            ),
-                            child: Slider(
-                              min: 0,
-                              max: value.totalDuration.inSeconds.toDouble(),
-                              value: value.currentDuration.inSeconds
-                                  .toDouble()
-                                  .clamp(0,
-                                      value.totalDuration.inSeconds.toDouble()),
-                              activeColor: Colors.green,
-                              onChanged: (double double) {
-                                //during when user is sliding around
-                                value.seek(Duration(seconds: double.toInt()));
-                              },
-                              onChangeEnd: (double double) {
-                                //sliding has finished, go to the position in song duration
-                                value.seek(Duration(seconds: double.toInt()));
-                              },
-                            ),
-                          ),
+                          Text(
+                            "1",
+                          )
                         ],
                       ),
-
-                      //playback controls
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-
-                //playback controls
-                Row(
-                  children: [
-                    //skip prev
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: value.playPrevSong,
-                        child: Neubox(
-                          child: Icon(Icons.skip_previous),
-                        ),
+                      Expanded(child: Container()),
+                      //set time
+                      TimerPicker(
+                        value: value,
                       ),
-                    ),
-                    const SizedBox(
-                      width: 20.0,
-                    ),
-                    //play pause
-                    Expanded(
-                      flex: 2,
-                      child: GestureDetector(
-                        onTap: value.pauseOrResume,
-                        child: Neubox(
-                          child: Icon(
-                              value.isPlaying ? Icons.pause : Icons.play_arrow),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 20.0,
-                    ),
-                    //skip forward
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: value.playNextSong,
-                        child: Neubox(
-                          child: Icon(Icons.skip_next),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                //like,share,cmt
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        //like
-                        IconButton(
-                          onPressed: value.favorites,
-                          icon: (value.isFavorite)
-                              ? Icon(Icons.favorite, color: Colors.red)
-                              : Icon(Icons.favorite_border),
-                        ),
-                        Text(
-                          "1",
-                        )
-                      ],
-                    ),
-                    Expanded(child: Container()),
-                    //set time
-                    TimerPicker(
-                      value: value,
-                    ),
-                    //share
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.share_rounded)),
-                    //comment
-                    CommentModal(),
-                    /*
+                      //share
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.share_rounded)),
+                      //comment
+                      CommentModal(),
+                      /*
                     Expanded(child: Container()),
                     GestureDetector(
                       child: Row(
@@ -269,13 +135,14 @@ class _FullPlayingViewState extends State<FullPlayingView> {
                         ],
                       ),
                     )*/
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      );
-    });
+        );
+      },
+    );
   }
 }
